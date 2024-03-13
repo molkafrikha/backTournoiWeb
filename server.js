f303919cef0f3f641b5549c7eca0a1527104dc7c
molkafrikha <124214031+molkafrikha@users.noreply.github.com>
2024-03-13 14:52:13 +0000

    port

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ db.mongoose
 
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8089;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
