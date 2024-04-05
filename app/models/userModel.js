eaf05913974599a5eb73c75856f412871dd8a4b5
molkafrikha <124214031+molkafrikha@users.noreply.github.com>
2024-04-05 02:29:38 +0100

    Add files via upload

---
+++ app/models/userModel.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userRoles = ['organizer', 'admin', 'player', 'coach', 'agent'];
+
+const UserSchema = new mongoose.Schema(
+  {
+    fullname: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    image : {
+      type : String,
+      required: false,
+    },
+    role: {
+      type: String,
+      enum: userRoles,
+      default: 'organizer', // Définir le rôle par défaut comme 'player'
+    },
+    
+    confirmed: {
+      type: Boolean,
+      default: false,
+    },
+    confirmedByAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    
+  },
+  {
+    timestamps: true,
+  }
+);
+
+UserSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+UserSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
